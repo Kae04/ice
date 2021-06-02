3464357216cf529e426f4f295cbb6220c6185e1d
Kae04 <kaede.kajiro@gmail.com>
2021-06-03 01:52:16 +0900

    first-commit

---
+++ aaa.php
@@ -0,0 +1,11 @@
+<?php
+$aa=["a","b","c"];
+
+// foreach($aa as $a){
+//     echo 111;
+//     echo $a;
+// }
+foreach($aa as $key => $a){
+    echo $key.'=>'.$a.'<br />';
+    
+}
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+$my_name = htmlspecialchars($_POST['my_name'], ENT_QUOTES);
+print $my_name . "さんご登録ありがとうございます";
+?>
